09eb74ca760b084142e1541ded38da08b5465e36
GaboUCR <gabrielgamboav19@gmail.com>
2025-03-25 12:26:12 -0600

    Added retardoVisual and generarAleatorio16 implementation
    Signed-off-by: GaboUCR <gabrielgamboav19@gmail.com>

--- lab1/src/dice.c
+++ lab1/src/dice.c
@@ -6,3 +6,28 @@ unsigned int __at 0x2007 __CONFIG = (_MCLRE_OFF & _WDTE_OFF);
 // Prototipos de funciones
 void generarAleatorio16(unsigned short *valorActual);
 void retardoVisual(unsigned int duracion);
+
+
+// Función para generar números pseudoaleatorios con LFSR de 16 bits
+void generarAleatorio16(unsigned short *valorActual) {
+    // Verifica si el bit menos significativo es 1
+    if ((*valorActual) & 1) {
+        // Desplaza a la derecha
+        (*valorActual) >>= 1;
+        // Hace XOR con la máscara para retroalimentar bits
+        (*valorActual) ^= (1 << 15) | (1 << 14) | (1 << 12) | (1 << 3);
+    } else {
+        // Desplaza a la derecha sin aplicar la máscara
+        (*valorActual) >>= 1;
+    }
+}
+
+// Función para crear un retardo basado en bucles
+void retardoVisual(unsigned int duracion) {
+    unsigned int i, j;
+    for (i = 0; i < duracion; i++) {
+        for (j = 0; j < 256; j++) {
+            // No se hace nada en este bucle interno
+        }
+    }
+}
